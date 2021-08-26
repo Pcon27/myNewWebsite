c8d274426fdc974957b834e2f3f9f53f68ab4785
pconsag <pconsagra@g.ucla.edu>
2021-08-25 17:44:31 -0700

    uodate styling for navbar and about page

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import resume from '../images/Consagra.Peter.Resume.pdf'
+import "../stylesheets/navbar.css"
 
 // TODO: Add a comment explaining how we are able to extract the key value pairs from props
 
@@ -9,58 +10,56 @@ function NavTabs({ currentPage, handlePageChange }) {
   return (
 
     <div>
-      <h1>Peter Consagra</h1>
-      <ul className="nav nav-tabs">
-      <li className="nav-item">
-          <a
-            href="#about"
-            onClick={() => handlePageChange('About')}
-            //  TODO: Add a comment explaining what this logic is doing
+      <section className="wtf">
+        <ul className="nav nav-tabs">
+        <h1 className="name">Peter Consagra</h1>
+          <li className="nav-item">
+              <a
+                href="#about"
+                onClick={() => handlePageChange('About')}
+                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+              >
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#portfolio"
+                onClick={() => handlePageChange('Portfolio')}
+                //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
 
-            // this is checking to see if we are on the 'about' page, if we are, the nav link to the 'about' page is active. if not, the nav bar won't take you to the 'about' page
+                // ternery operator, checking to see if we are on the homepage
 
-            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-          >
-            About Me
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#portfolio"
-            onClick={() => handlePageChange('Portfolio')}
-            //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
+                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href={resume}
+                onClick={() => handlePageChange('Resume')}
+                //  TODO: Add a comment explaining what this logic is doing
 
-            // ternery operator, checking to see if we are on the homepage
-
-            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-          >
-            Portfolio
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href={resume}
-            onClick={() => handlePageChange('Resume')}
-            //  TODO: Add a comment explaining what this logic is doing
-
-            // this is checking to see if we are on the 'blog' page, if we are, the nav link to the 'about' page is active. if not, the nav bar won't take you to the 'blog' page
-            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-          >
-            Resume
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#contact"
-            //  TODO: Add a comment explaining what this logic is doing
-            // this is checking to see if we are on the 'contact' page, if we are, the nav link to the 'about' page is active. if not, the nav bar won't take you to the 'contact' page
-            onClick={() => handlePageChange('Contact')}
-            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-          >
-            Contact
-          </a>
-        </li>
-      </ul>
+                // this is checking to see if we are on the 'blog' page, if we are, the nav link to the 'about' page is active. if not, the nav bar won't take you to the 'blog' page
+                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+              >
+                Resume
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#contact"
+                //  TODO: Add a comment explaining what this logic is doing
+                // this is checking to see if we are on the 'contact' page, if we are, the nav link to the 'about' page is active. if not, the nav bar won't take you to the 'contact' page
+                onClick={() => handlePageChange('Contact')}
+                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+              >
+                Contact
+              </a>
+            </li>
+        </ul>
+        </section>
     </div>
   );
 }
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,33 +1,18 @@
 import React from 'react';
 import peteImage from '../../images/me.jpg'
+import "../../stylesheets/about.css"
 
-const styles = {
-  card: {
-    margin: 20,
-    background: '#e8eaf6',
-  },
-  heading: {
-    background: '#3f51b5',
-    minHeight: 50,
-    lineHeight: 3.5,
-    fontSize: '1.2rem',
-    color: 'blue',
-    padding: '0 20px',
-  },
-  content: {
-    padding: 20,
-    color: 'black'
-  },
-};
 
 export default function About() {
   return (
-    <div style={styles.content}>
-      <h1>About Me</h1>
-      <img id="peteImage" src={peteImage} alt="peteImage"></img>
-      <p>
-      I am a current Senior at the University of California, Los Angeles (UCLA) pursuing a degree in Political Science. I am also currently enrolled in the UC Berkely Extensions Full Stack Developer Boot Camp, and hope to persue product development in the tech industry when I complete my studies. I am currently seeking a role in web development, UX/UI design, software development, or Product Management. I enjoy playing tennis, woodworinking, hiking, and supporting the sports teams from my local San Francisco in my free time.
-      </p>
+    <div className="div">
+      <h1 id="title">About Me</h1>
+      <section className="info">
+        <img id="peteImage" src={peteImage} alt="peteImage"></img>
+        <p>
+        I am a current Senior at the University of California, Los Angeles (UCLA) pursuing a degree in Political Science. I am also currently enrolled in the UC Berkely Extensions Full Stack Developer Boot Camp, and hope to persue product development in the tech industry when I complete my studies. I am currently seeking a role in web development, UX/UI design, software development, or Product Management. I enjoy playing tennis, woodworinking, hiking, and supporting the sports teams from my local San Francisco in my free time.
+        </p>
+      </section>
     </div>
   );
 }
