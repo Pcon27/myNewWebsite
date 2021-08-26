904939965cc0e5e85951cb6f1ec1b44cb1cd2f4a
pconsag <pconsagra@g.ucla.edu>
2021-08-25 18:18:09 -0700

    add styling to portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,30 +2,58 @@ import React from 'react';
 import jamMapImg from '../../images/Jam-Map.png';
 import weatherTracker from '../../images/Weather-Tracker.png';
 import reciplease from '../../images/Reciplease.png';
+import budgetTracker from '../../images/budgetTracker.png'
+import workoutTracker from '../../images/WorkoutTracker.png'
+import passwordGenerator from '../../images/passwordGenerator.png'
+import '../../stylesheets/portfolio.css'
 
 export default function Home() {
   return (
 <div>
   <h2 id="portfolio">Portfolio</h2>
-  <section id="project1">
-        <label>Jam Map Project <a href="https://github.com/PDPco/jam-map">Repo</a></label>
-        <a href="https://pdpco.github.io/jam-map/">
-            <img id="JMProject" src={jamMapImg} atl="JamMap"/>
-        </a>
-  </section>
 
-  <section id="otherProjects">
-        <div id="weatherTracker" class="SMLPJ">
+
+  <section id="otherProjects" className="SMLPJ">
+
+        <div id="project1" className="SMLPJ">
+                <label>Jam Map Project <a href="https://github.com/PDPco/jam-map">Repo</a></label>
+                <a href="https://pdpco.github.io/jam-map/">
+                    <img className="SMLIMG" src={jamMapImg} atl="JamMap"/>
+                </a>
+        </div>
+
+        <div id="weatherTracker" className="SMLPJ">
             <label>Weather Tracker Project <a href="https://github.com/Pcon27/Weather_Tracker_Repo">Repo</a></label>
             <a href="https://pcon27.github.io/Weather_Tracker_Repo/">
-                <img class="SMLIMG" src={weatherTracker} atl="Weather Tracker Project"/>
+                <img className="SMLIMG" src={weatherTracker} atl="Weather Tracker Project"/>
             </a>
         </div>
 
-        <div id="PWGen" class="SMLPJ">
+        <div id="Reciplease" className="SMLPJ">
             <label>Reciplease <a href="https://github.com/Pcon27/Reciplease#deployed">Repo</a></label>
             <a href="https://pacific-caverns-03345.herokuapp.com">
-                <img class="SMLIMG" src={reciplease} atl="Password-Generator Project"/>
+                <img className="SMLIMG" src={reciplease} atl="Password-Generator Project"/>
+            </a>
+        </div>
+
+        <div id="BudgetTracker" className="SMLPJ">
+            <label>Budget Tracker Project <a href="https://github.com/Pcon27/BudgetTracker">Repo</a></label>
+            <a href="https://quiet-wildwood-99872.herokuapp.com/">
+                <img className="SMLIMG" src={budgetTracker} atl="Weather Tracker Project"/>
+            </a>
+        </div>
+
+        <div id="PWGen" className="SMLPJ">
+            <label>Password Generator Project <a href="https://github.com/Pcon27/Generate-a-password-repo">Repo</a></label>
+            <a href="https://pcon27.github.io/Generate-a-password-repo/">
+                <img className="SMLIMG" src={passwordGenerator} atl="Weather Tracker Project"/>
+            </a>
+        </div>
+
+        <div id="workoutTracker" className="SMLPJ">
+            <label>Workout Tracker Project <a href="https://github.com/Pcon27/Fitness_Tracker">Repo</a></label>
+            <a href="https://quiet-bayou-47430.herokuapp.com">
+                <img className="SMLIMG" src={workoutTracker} atl="Weather Tracker Project"/>
             </a>
         </div>
     </section>
