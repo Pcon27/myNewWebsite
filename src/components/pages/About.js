670251328d29f581a1022f404475841de1306969
pconsag <pconsagra@g.ucla.edu>
2021-09-19 22:11:38 -0700

    update resume and bio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,7 +10,7 @@ export default function About() {
       <section className="info">
         <img id="peteImage" src={peteImage} alt="peteImage"></img>
         <p>
-        I am a current Senior at the University of California, Los Angeles (UCLA) pursuing a degree in Political Science. I am also currently enrolled in the UC Berkely Extensions Full Stack Developer Boot Camp, and hope to persue product development in the tech industry when I complete my studies. I am currently seeking a role in web development, UX/UI design, software development, or Product Management. I enjoy playing tennis, woodworinking, hiking, and supporting the sports teams from my local San Francisco in my free time.
+        Full Stack Web Developer and Senior at University of California, Los Angeles (UCLA) persuing a BS in Political Science. Effective at combining creativity and problem solving to develop user-friendly applications. Known to have a strong interest in intuitive user stories and product UX. 
         </p>
       </section>
 
